2f4c8d6f0d1b1092eb0b6ec6797d16b7124780cb
Joshua <joshua@lupecode.com>
2018-04-25 21:13:44 -0700

    Wrote a simple Monty Hall paradox tester.

---
+++ mh.php
@@ -0,0 +1,25 @@
+<?php
+
+$Runs         = 10000000;
+$PrintDivisor = 1000000;
+$Options      = ["Door 1", "Door 2", "Door 3",];
+$Correct      = 0;
+srand(time());
+
+for ($iteratorRun = 1; $iteratorRun < $Runs; $iteratorRun++) {
+    $Chosen = $Options[array_rand($Options)];
+    $Winner = $Options[array_rand($Options)];
+    // Reveal a loser door.
+    $rDiff = array_diff($Options, [$Winner, $Chosen]);
+    $Reveal = $rDiff[array_rand($rDiff)];
+    // Switch to a remaining door.
+    $sDiff= array_diff($Options, [$Reveal, $Chosen]);
+    $Switch = $sDiff[array_rand($sDiff)];
+    if ($Switch === $Winner) {
+        $Correct++;
+    }
+    if ($iteratorRun % $PrintDivisor === 0) {
+        echo "After $iteratorRun rounds we won $Correct times! (" . round($Correct / $iteratorRun * 100, 5) . "%)" . PHP_EOL;
+    }
+}
+echo "After $Runs rounds we won $Correct times! (" . round($Correct / $Runs * 100, 5) . "%)" . PHP_EOL;
